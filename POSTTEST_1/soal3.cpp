5c991c7dbfca890467f51ecc669abf91ce33615a
Muhamad Farid <muhamadfaridalmubarok2002@gmail.com>
2026-03-01 13:50:53 +0800

    Finish Soal 3

---
+++ POSTTEST_1/soal3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int arr[7] = {2, 3, 5, 7, 11, 13, 17};
+
+void reverseArray(int *ptrArr, int n) {
+
+    int *ptrAwal = arr;
+    int *ptrAkhir = arr + n - 1;
+
+    while (ptrAwal < ptrAkhir) {
+        int temp = *ptrAwal;
+        *ptrAwal = *ptrAkhir;
+        *ptrAkhir = temp;
+
+        ptrAwal++;
+        ptrAkhir--;
+    }
+
+}
+
+int main () {
+    string judul = R"(
+====================================
+|                                  |
+|  Soal 3 (MEMBALIK URUTAN ARRAY)  |
+|                                  |
+====================================)";
+
+    cout << judul << endl;
+    cout << "\nArray Sebelum Dibalik: ";
+    for (int i = 0; i < 7; i++) {
+        cout << arr[i] << " ";
+    }
+
+    reverseArray(arr, 7);
+    cout << "\nArray Sesudah Dibalik: ";
+    for (int i = 0; i < 7; i++) {
+        cout << arr[i] << " ";
+    }
+
+    cout << endl;
+    
+    return 0;
+}
