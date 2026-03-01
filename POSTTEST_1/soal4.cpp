e5254da95517dedc517acbd5d03b0d9802c9dc06
Muhamad Farid <muhamadfaridalmubarok2002@gmail.com>
2026-03-01 13:15:00 +0800

    Finish Soal 4

---
+++ POSTTEST_1/soal4.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void tukar (int *ptrNilaiA, int *ptrNilaiB) {
+
+    int temp = *ptrNilaiA;
+    *ptrNilaiA = *ptrNilaiB;
+    *ptrNilaiB = temp;
+
+}
+
+int main () {
+
+    int nilaiA, nilaiB;
+
+    system("cls");
+
+    string judul = R"(
+/============================\
+||                          ||
+||  MENUKAR NILAI VARIABEL  ||
+||                          ||
+\============================/)";
+
+    cout << judul << endl;
+    cout << "\nMasukkan Nilai Pertama: ";
+    cin >> nilaiA;
+    cout << "Masukkan Nilai Kedua: ";
+    cin >> nilaiB;
+
+    cout << "\n--- Hasil Nilai Sebelum Ditukar ---" << endl;
+    cout << "Nilai Pertama: " << nilaiA << endl;
+    cout << "Nilai Kedua: " << nilaiB << endl;
+    
+    tukar(&nilaiA, &nilaiB);
+    cout << "\n--- Hasil Nilai Sesudah Ditukar ---" << endl;
+    cout << "Nilai Pertama: " << nilaiA << endl;
+    cout << "Nilai Kedua: " << nilaiB << endl;
+    cout << '\n';
+
+    return 0;
+}
