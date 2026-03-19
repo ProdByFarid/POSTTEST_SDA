9372ec03f818d9009a3411d99fd5cc20c2729dae
Muhamad Farid <muhamadfaridalmubarok2002@gmail.com>
2026-03-19 23:30:38 +0800

    chore: Tambahkan Error Handling Di Opsi 1

--- POSTTEST_2/2509106087_MuhamadFaridAlMubarok_pt2.cpp
+++ POSTTEST_2/2509106087_MuhamadFaridAlMubarok_pt2.cpp
@@ -4,8 +4,6 @@
 using namespace std;
 
 #define MAKS_DATA 50
-Hewan dataHewan[MAKS_DATA];
-int jumlahData = 0;
 
 struct Hewan {
     int idHewan;
@@ -13,6 +11,9 @@ struct Hewan {
     string jenisHewan;
     int hargaHewan;
 };
+
+int jumlahData = 0;
+Hewan dataHewan[MAKS_DATA];
 
 void jeda() {
     cout << "\n[Tekan Enter Untuk Melanjutkan...]" << endl;
@@ -24,7 +25,6 @@ void tambahHewan() {
         cout << "\n(!) Kapasitas Data Penuh!" << endl;
         jeda();
     } else {
-
         Hewan *ptrHewan = &dataHewan[jumlahData];
 
         cout << "\n--- Tambah Data Hewan ---" << endl;
@@ -46,26 +46,30 @@ void tambahHewan() {
 }
 
 void lihatData() {
-    string header = "==========================================================================";
+    string header = "=================================================================================";
 
     if (jumlahData == 0) {
-        cout << "\n (!) Belum Ada Data Yang Ditambhkan" << endl;
+        cout << "\n (!) Belum Ada Data Yang Ditambahkan" << endl;
         jeda();
     } else {
-        cout << "--- Lihat Data ---" << endl;
+        cout << "\n--- Lihat Data ---" << endl;
 
         cout << header << endl;
-        cout << left << setw(5) << "ID" << setw(15) << "Nama" << setw(15) << "Jenis" 
-             << setw(15) << "Harga" << endl;
+        cout << "| " << setw(4) << left << "ID"
+            << "| " << setw(25) << left << "Nama"
+            << "| " << setw(30) << left << "Jenis"
+            << "| " << setw(12) << left << "Harga" << " |" << endl;
+
         cout << header << endl;
 
         for (Hewan *ptr = dataHewan; ptr < dataHewan + jumlahData; ptr++) {
-            cout << left << setw(5) << (*ptr).idHewan 
-            << setw(15) << (*ptr).namaHewan 
-            << setw(15) << (*ptr).jenisHewan 
-            << setw(15) << "Rp " << (*ptr).hargaHewan << endl;
+            cout << "| " << left << setw(4) << (*ptr).idHewan 
+            << "| " << setw(25) << (*ptr).namaHewan 
+            << "| " << setw(30) << (*ptr).jenisHewan 
+            << "| Rp" << setw(10) << (*ptr).hargaHewan << " |" << endl;
         }
         cout << header << endl;
+        jeda();
     }
 }
 
@@ -81,16 +85,17 @@ void bubbleSortNama() {
         jeda();
     } else {
         for (int i = 0; i < jumlahData - 1; i++) {
-            for (int j = 0; jumlahData - i - 1; j++) {
-                Hewan *ptr1 = dataHewan[j];
-                Hewan *ptr2 = dataHewan[j + 1];
+            for (int j = 0; j < jumlahData - i - 1; j++) {
+                Hewan *ptr1 = &dataHewan[j];
+                Hewan *ptr2 = &dataHewan[j + 1];
 
                 if ((*ptr1).namaHewan > (*ptr2).namaHewan) {
                     tukar(ptr1, ptr2);
                 }
             }
         } 
-        cout << "(+) Data Berhasil Diurutkan Berdasarkan Nama (A-Z)" << endl;
+        cout << "\n(+) Data Berhasil Diurutkan Berdasarkan Nama (A-Z)" << endl;
+        cout << "(+) Silahkan Cek Di Pilihan Nomor 2" << endl;
         jeda();
     }
 }
@@ -101,19 +106,17 @@ void bubbleSortId() {
         jeda();
     } else {
         for (int i = 0; i < jumlahData - 1; i++) {
-            for (int j = 0; jumlahData - i - 1; j++) {
-                Hewan *ptr1 = dataHewan[j];
-                Hewan *ptr2 = dataHewan[j + 1];
+            for (int j = 0; j < jumlahData - i - 1; j++) {
+                Hewan *ptr1 = &dataHewan[j];
+                Hewan *ptr2 = &dataHewan[j + 1];
 
                 if ((*ptr1).idHewan > (*ptr2).idHewan) {
                     tukar(ptr1, ptr2);
                 }
             }
         } 
-        cout << "(+) Data Berhasil Diurutkan Berdasarkan ID" << endl;
     }
 }
-
 
 void selectionSortHarga() {
     if (jumlahData == 0) {
@@ -124,8 +127,8 @@ void selectionSortHarga() {
             int indeksMin = i;
             
             for (int j = i + 1; j < jumlahData; j++) {
-                Hewan *ptr1 = dataHewan[indeksMin];
-                Hewan *ptr2 = dataHewan[j];
+                Hewan *ptr1 = &dataHewan[indeksMin];
+                Hewan *ptr2 = &dataHewan[j];
                 
                 if ((*ptr2).hargaHewan < (*ptr1).hargaHewan) {
                     indeksMin = j;
@@ -133,7 +136,8 @@ void selectionSortHarga() {
             }
             tukar(&dataHewan[indeksMin], &dataHewan[i]);
         }
-        cout << "(+) Data Berhasil Diurutkan Berdasarkan Harga" << endl;
+        cout << "\n(+) Data Berhasil Diurutkan Berdasarkan Harga" << endl;
+        cout << "(+) Silahkan Cek Di Pilihan Nomor 2" << endl;
         jeda();
     }
 }
@@ -145,25 +149,26 @@ void linearSearchNama() {
     getline(cin, target);
 
     bool ada = false;
-    Hewan *ptr1 = &dataHewan;
+    Hewan *ptr1 = dataHewan;
     Hewan *ptr2 = dataHewan + jumlahData;
 
     while (ptr1 < ptr2) {
         if (target == (*ptr1).namaHewan) {
             cout << "\n(+) Data Ditemukan!" << endl;
-            cout << "\nID Hewan: " << (*ptr1).idHewan << endl;
-            cout << "\nNama Hewan  : " << (*ptr1).namaHewan << endl;
-            cout << "\nJenis Hewan : " << (*ptr1).jenisHewan << endl;
-            cout << "\nHarga Hewan : " << (*ptr1).hargaHewan << endl;
-
+            cout << "\nID Hewan    : " << (*ptr1).idHewan << endl;
+            cout << "Nama Hewan  : " << (*ptr1).namaHewan << endl;
+            cout << "Jenis Hewan : " << (*ptr1).jenisHewan << endl;
+            cout << "Harga Hewan : Rp" << (*ptr1).hargaHewan << endl;
             ada = true;
             break;
         }
+        ptr1++;
     }
 
     if (!ada) {
-        cout << "(-) Data Tidak Ditemukan!" << endl;
+        cout << "\n(-) Data Tidak Ditemukan!" << endl;
     }
+    jeda();
 }
 
 void fibbonaciSearchId() {
@@ -172,56 +177,72 @@ void fibbonaciSearchId() {
     if (jumlahData == 0) {
         cout << "(!) Tidak Ada Data Untuk Dicari!" << endl;
         jeda();
-    } else {
-        bubbleSortId();
+        return;
+    }
+    
+    bubbleSortId();
 
-        cout << "\nCari ID Hewan: ";
-        cin >> target;
+    cout << "\nCari ID Hewan: ";
+    cin >> target;
 
-        int fib2 = 0;
-        int fib1 = 1;
-        int fib = fib2 + fib1;
+    int fib2 = 0;
+    int fib1 = 1;
+    int fib = fib2 + fib1;
 
-        while (fib < jumlahData) {
-            fib2 = fib1;
-            fib1 = fib;
-            fib = fib2 + fib1;
+    while (fib < jumlahData) {
+        fib2 = fib1;
+        fib1 = fib;
+        fib = fib2 + fib1;
+    }
+
+    int offset = -1;
+    bool ada = false;
+    Hewan *ptr;
+
+    while (fib > 1) {
+        int i = offset + fib2;
+        if (i > jumlahData - 1) {
+            i = jumlahData - 1;
         }
 
-        int offset = -1;
-        bool ada = false;
-        Hewan *ptr;
+        ptr = &dataHewan[i]; 
 
-        while (fib > 1) {
-
-            int i = min(offset + fib2, jumlahData - 1);
-
-            ptr = &dataHewan[i]; 
-
-            if ((*ptr).idHewan < target) {
-                fib = fib1;
-                fib1 = fib2;
-                fib2 = fib - fib1;
-                offset = i;
-            } else if ((*ptr).idHewan > target) {
-                fib = fib2;
-                fib1 = fib1 - fib2;
-                fib2 = fib - fib1;
-            } else {
-                cout << "\n(+) Data Ditemukan!" << endl;
-                cout << "\nID Hewan: " << (*ptr).idHewan << endl;
-                cout << "\nNama Hewan  : " << (*ptr).namaHewan << endl;
-                cout << "\nJenis Hewan : " << (*ptr).jenisHewan << endl;
-                cout << "\nHarga Hewan : " << (*ptr).hargaHewan << endl;
-
-                ada = true;
-                break;
-            }
-
-                return;
-            
+        if ((*ptr).idHewan < target) {
+            fib = fib1;
+            fib1 = fib2;
+            fib2 = fib - fib1;
+            offset = i;
+        } else if ((*ptr).idHewan > target) {
+            fib = fib2;
+            fib1 = fib1 - fib2;
+            fib2 = fib - fib1;
+        } else {
+            cout << "\n(+) Data Ditemukan!" << endl;
+            cout << "\nID Hewan    : " << (*ptr).idHewan << endl;
+            cout << "Nama Hewan  : " << (*ptr).namaHewan << endl;
+            cout << "Jenis Hewan : " << (*ptr).jenisHewan << endl;
+            cout << "Harga Hewan : Rp" << (*ptr).hargaHewan << endl;
+            ada = true;
+            break;
         }
     }
+
+    if (!ada && fib1 && offset < jumlahData - 1) {
+        ptr = &dataHewan[offset + 1];
+        if ((*ptr).idHewan == target) {
+            cout << "\n(+) Data Ditemukan!" << endl;
+            cout << "\nID Hewan    : " << (*ptr).idHewan << endl;
+            cout << "Nama Hewan  : " << (*ptr).namaHewan << endl;
+            cout << "Jenis Hewan : " << (*ptr).jenisHewan << endl;
+            cout << "Harga Hewan : Rp" << (*ptr).hargaHewan << endl;
+            ada = true;
+        }
+    }
+
+    if (!ada) {
+        cout << "\n(-) Data Tidak Ditemukan!" << endl;
+    }
+    jeda();
 }
 
 void menuUtama() {
@@ -231,7 +252,7 @@ void menuUtama() {
         system("cls");
 
         cout << header << endl;
-        cout << "     Pawcare Petshop & Dokter Hewan     " << endl;
+        cout << "|    Pawcare Petshop & Dokter Hewan    |" << endl;
         cout << header << endl;
         cout << "|  [1]. Tambah Data Hewan Baru         |" << endl;
         cout << "|  [2]. Tampilkan Semua Data Hewan     |" << endl;
@@ -245,24 +266,39 @@ void menuUtama() {
         cin >> pilihan;
 
         if (pilihan == '1') {
-
+            tambahHewan();
+        } else if (pilihan == '2') {
+            lihatData();
+        } else if (pilihan == '3') {
+            linearSearchNama();
+        } else if (pilihan == '4') {
+            fibbonaciSearchId();
+        } else if (pilihan == '5') {
+            bubbleSortNama();
+        } else if (pilihan == '6') {
+            selectionSortHarga();
+        } else if (pilihan == '7') {
+            cout << "\nTerima Kasih Telah Menggunakan Program Ini!" << endl;
+            jeda();
+            break;
+        } else {
+            cout << "\n(!) Pilihan Tidak Valid!" << endl;
+            jeda();
         }
     } while (pilihan != '7');
-    
 }
 
 int main() {
-
-    dataHewan[0] = {74, "Beef", "Chihuahua", 450000};
-    dataHewan[1] = {13, "Nemo", "Ikan Badut", 1300000};
-    dataHewan[2] = {22, "Little One", "Ginger Cat", 300000};
-    dataHewan[3] = {31, "Gary", "Siput Laut", 580000};
-    dataHewan[4] = {26, "Kuma The Destoyer", "Beruang Grizly", 7600000};
-    dataHewan[5] = {2, "Thomas", "Russian Grayish-Blue.", 9000000};
-    dataHewan[6] = {7, "Jerry", "Tikus", 1500000};
-    dataHewan[7] = {16, "Punch", "Japanese Macqque", 3300000};
+    dataHewan[0] = {74, "Beef", "Chihuahua", 850000};
+    dataHewan[1] = {13, "Ciko", "Ikan Badut", 800000};
+    dataHewan[2] = {22, "Fluxx", "Ginger Cat", 650000};
+    dataHewan[3] = {31, "Gary", "Siput Laut", 600000};
+    dataHewan[4] = {26, "Kuma The Destoyer", "Beruang Grizly", 450000};
+    dataHewan[5] = {2, "Heli", "Russian Grayish-Blue", 550000};
+    dataHewan[6] = {7, "Emily", "Tikus", 700000};
+    dataHewan[7] = {16, "Dune", "Japanese Macaque", 750000};
     dataHewan[8] = {45, "Judy", "Kelinci", 500000};
-    dataHewan[9] = {67, "Nick", "Rubah", 1550000};
+    dataHewan[9] = {67, "Astra", "Rubah", 900000};
     jumlahData = 10;
 
     menuUtama();
